5272c88502e03f3d2611b0500aba0e209891bace
1121206561 <1121206561@qq.com>
2020-01-15 15:40:09 +0800

    分支修改

--- src/main/java/cn/wolfcode/git/Person.java
+++ src/main/java/cn/wolfcode/git/Person.java
@@ -3,8 +3,8 @@ package cn.wolfcode.git;
 public class Person {
 
 	public Person() {
-		System.out.println("第一次修改");
 		System.out.println("第二次修改");
+		System.out.println("第一次修改");
 		System.out.println("分支上的修改");
 	}
 
