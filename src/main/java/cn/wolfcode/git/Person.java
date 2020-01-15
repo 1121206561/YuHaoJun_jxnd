0f8e7f9a16e11a9de6e38e43fbc4125f0adc266f
1121206561 <1121206561@qq.com>
2020-01-15 14:35:09 +0800

    My first commit in Eclipse

---
+++ src/main/java/cn/wolfcode/git/Person.java
@@ -0,0 +1,9 @@
+package cn.wolfcode.git;
+
+public class Person {
+
+	public Person() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/main/java/cn/wolfcode/git/User.java
@@ -0,0 +1,9 @@
+package cn.wolfcode.git;
+
+public class User {
+
+	public User() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
