98ee54576fbff5f5bcb3d496acb9a1643a9151c8
1121206561 <1121206561@qq.com>
2020-01-15 15:08:33 +0800

    My third edit

--- src/main/java/cn/wolfcode/git/Person.java
+++ src/main/java/cn/wolfcode/git/Person.java
@@ -3,11 +3,8 @@ package cn.wolfcode.git;
 public class Person {
 
 	public Person() {
-<<<<<<< HEAD
 		System.out.println("我的第二次修改");
-=======
 		System.out.println("第一次修改");
->>>>>>> branch 'master' of https://github.com/1121206561/YuHaoJun_jxnd.git
 	}
 
 }
